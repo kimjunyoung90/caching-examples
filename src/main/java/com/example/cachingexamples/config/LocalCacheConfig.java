bf0e163d5f9731040ba2aae5ab1939a24708771b
junyoungKim <wnsdud1900427@gmail.com>
2025-12-13 20:50:56 +0900

    fix : local cache 설정 수정

--- src/main/java/com/example/cachingexamples/config/LocalCacheConfig.java
+++ src/main/java/com/example/cachingexamples/config/LocalCacheConfig.java
@@ -1,26 +1,37 @@
 package com.example.cachingexamples.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
-@EnableCaching
 @Profile("local")
+@EnableCaching
 @Configuration
 public class LocalCacheConfig {
      @Bean
-     public CaffeineCacheManager cacheManager() {
-         CaffeineCacheManager cacheManager = new CaffeineCacheManager("localCache");
-         cacheManager.setCaffeine(
+     public CacheManager cacheManager() {
+
+         CaffeineCache caffeineCache = new CaffeineCache(
+                 "popular_posts_cache", //@Cacheable에서 사용할 캐시 이름
                  Caffeine.newBuilder()
-                         .maximumSize(10000)
-                         .expireAfterWrite(Duration.ofMinutes(10)) // 10분
+                         .expireAfterWrite(5, TimeUnit.MINUTES)
+                         .maximumSize(100)
+                         .build()
          );
+
+         //SimpleCacheManager에 등록한 캐시를 등록
+         SimpleCacheManager cacheManager = new SimpleCacheManager();
+         cacheManager.setCaches(Arrays.asList(caffeineCache));
          return cacheManager;
      }
 }
